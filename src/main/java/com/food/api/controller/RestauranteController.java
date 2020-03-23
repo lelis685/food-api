4444d72fda3f93044db307ad5eb578be23e174f6
lelis <lelis685@gmail.com>
2020-03-22 22:14:39 -0300

    add novas entidades

--- src/main/java/com/food/api/controller/RestauranteController.java
+++ src/main/java/com/food/api/controller/RestauranteController.java
@@ -92,7 +92,7 @@ public class RestauranteController {
 			Restaurante restauranteAtual = restauranteRepository.findById(restauranteId).orElse(null);
 
 			if (restauranteAtual != null) {
-				BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id","formasPagamento","endereco","dataCadastro","produtos");
 
 				restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
 				return ResponseEntity.ok(restauranteAtual);
