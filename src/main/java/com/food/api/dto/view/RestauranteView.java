ddcf9d733d5365259f39631e762af0b6fe9fd0f0
lelis <lelis685@gmail.com>
2020-06-09 22:37:35 -0300

    refatoração

--- src/main/java/com/food/api/dto/view/RestauranteView.java
+++
@@ -1,9 +0,0 @@
-package com.food.api.dto.view;
-
-public interface RestauranteView {
-
-	public interface Resumo{}
-	
-	public interface ApenasNome{}
-	
-}
--- src/main/java/com/food/domain/service/VendaReportService.java
+++
@@ -1,9 +0,0 @@
-package com.food.domain.service;
-
-import com.food.domain.filter.VendaDiariaFilter;
-
-public interface VendaReportService {
-	
-	byte[] consultaVendasDiarias(VendaDiariaFilter filter, String timeOffset);
-
-}
